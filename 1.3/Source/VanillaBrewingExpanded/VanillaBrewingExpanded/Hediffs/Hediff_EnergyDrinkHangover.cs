0abc8d2d4bdc5486a1d81b3d85efd9a1167274d1
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-09-17 16:00:31 +0200

    Initial commit

---
+++ 1.3/Source/VanillaBrewingExpanded/VanillaBrewingExpanded/Hediffs/Hediff_EnergyDrinkHangover.cs
@@ -0,0 +1,16 @@
+﻿using RimWorld;
+using Verse;
+
+namespace VanillaBrewingExpanded
+{
+    public class Hediff_EnergyDrinkHangover : HediffWithComps
+    {
+        
+
+        public override void PostAdd(DamageInfo? dinfo)
+        {
+            base.PostAdd(dinfo);
+            this.pawn.needs.rest.CurLevel -= 0.2f;
+        }
+    }
+}
---
+++ 1.3/Source/VanillaBrewingExpanded/VanillaBrewingExpanded/Plants/Plant_AutoProduce.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace VanillaBrewingExpanded
+{
+    public class Plant_AutoProduce : Plant
+    {
+        public int counter = 0;
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+      
+            Scribe_Values.Look<int>(ref this.counter, "counter", 0, false);
+
+        }
+
+        public override void TickLong()
+        {
+            base.TickLong();
+            counter++;
+           // Log.Message(counter.ToString());
+            if (counter>60 && this.Growth>0.7) {
+                counter = 0;
+                System.Random random = new System.Random();
+                if (random.NextDouble() > 0.4) {
+                    Thing thing = ThingMaker.MakeThing(this.def.plant.harvestedThingDef, null);
+                    thing.SetForbidden(true);
+                    if (random.NextDouble() < 0.4)
+                    {
+                        thing.stackCount = 2;
+                    } else {
+                        thing.stackCount = 4;
+                    }                       
+                    GenPlace.TryPlaceThing(thing, this.Position+IntVec3.South, this.Map, ThingPlaceMode.Near, null, null, default(Rot4));
+                }
+                
+
+            }
+        }
+
+      
+    }
+}
