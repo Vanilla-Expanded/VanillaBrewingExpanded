61c29c5763f4ddfb724b417672adaeb7785e7857
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-06-12 09:33:07 +0200

    v4.0

---
+++ 1.6/Source/VanillaBrewingExpanded/VanillaBrewingExpanded/IngestionOutcomeDoers/IngestionOutcomeDoer_GiveHediffWithQualityFilter.cs
@@ -0,0 +1,52 @@
+﻿
+using System.Collections.Generic;
+using RimWorld;
+using Verse;
+namespace VanillaBrewingExpanded
+{
+    public class IngestionOutcomeDoer_GiveHediffWithQualityFilter : IngestionOutcomeDoer
+    {
+        public HediffDef hediffDef;
+
+        public float severity = -1f;
+
+        public ChemicalDef toleranceChemical;
+
+        private bool divideByBodySize;
+
+        public bool multiplyByGeneToleranceFactors;
+
+        public QualityCategory minQuality;
+
+        protected override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested, int ingestedCount)
+        {
+            CompQuality comp = ingested.TryGetComp<CompQuality>();
+            if (comp != null) { 
+                if(comp.Quality>= minQuality)
+                {
+                    Hediff hediff = HediffMaker.MakeHediff(hediffDef, pawn);
+                    float effect = ((!(severity > 0f)) ? hediffDef.initialSeverity : severity);
+                    AddictionUtility.ModifyChemicalEffectForToleranceAndBodySize(pawn, toleranceChemical, ref effect, multiplyByGeneToleranceFactors, divideByBodySize);
+                    hediff.Severity = effect;
+                    pawn.health.AddHediff(hediff);
+                    pawn.mindState.inspirationHandler.TryStartInspiration(InspirationDefOf.Inspired_Creativity);
+                }
+            
+            }
+
+            
+        }
+
+        public override IEnumerable<StatDrawEntry> SpecialDisplayStats(ThingDef parentDef)
+        {
+            if (!parentDef.IsDrug || !(chance >= 1f))
+            {
+                yield break;
+            }
+            foreach (StatDrawEntry item in hediffDef.SpecialDisplayStats(StatRequest.ForEmpty()))
+            {
+                yield return item;
+            }
+        }
+    }
+}
